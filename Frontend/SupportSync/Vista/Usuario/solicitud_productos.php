4429e20374ddd79c438172e67e6528755ed7f302
Daniel-Mendez-G09 <165663616+Daniel-Mendez-G09@users.noreply.github.com>
2024-04-09 06:34:48 -0500

    Add files via upload

---
+++ Frontend/SupportSync/Vista/Usuario/solicitud_productos.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../Img/img_6.png"> 
+    <title>Support Sync | Escalar Caso </title>
+
+    <link rel="stylesheet" href="../../Css/global_estilo.css">
+    <link rel="stylesheet" href="../../Css/Usuario/usuario_global_estilo.css">
+    <link rel="stylesheet" href="../../Css/Usuario/solicitud_producto_estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="box_nav">
+      <img src="../../Img/img_3.png" alt="">
+    </nav>
+
+    <section class="box_contenedor_general">
+        <section class="box_titulo">
+            <h2 class="titulo_dos">Productos</h2>
+        </section>
+            <div class="row box_nueva_solicitud">
+                <div class="col col_borde_derecha">
+                <h2 class="titulo_uno">Adquisición de productos</h2>
+                    <div class="d-flex justify-content-center box_btn_solicitud">
+                        <button type="submit" class="btn btn-dark">Sistema Interno 1</button>
+                    </div>
+                    <div class="d-flex justify-content-center box_btn_solicitud">
+                        <button type="submit" class="btn btn-dark">Sistema Interno 2</button>
+                    </div>
+                </div>
+                <div class="col">
+                    <h2 class="titulo_uno">Información de Productos</h2>
+                    <ul>
+                        <li>Tarjeta de Credito 1</li>
+                        <li>Tarjeta de Credito 2</li>
+                        <li>Cuenta de ahorros 1</li>
+                    </ul>
+                </div>
+            </div>
+    </section>
+
+    <div class="row box_btn_escalar">
+        <div class="col">
+            <div class="d-flex justify-content-center box_btn_solicitud">
+                <button type="submit" class="btn btn-danger">Finalizar Consulta</button>
+            </div>
+        </div>
+        <div class="col">
+            <div class="d-flex justify-content-center box_btn_solicitud">
+                <button type="submit" class="btn btn-danger">Escalar Caso</button>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
