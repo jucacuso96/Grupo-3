71b4a87e85a9fe4e924a421d896acbe8971cb163
Daniel-Mendez-G09 <165663616+Daniel-Mendez-G09@users.noreply.github.com>
2024-04-08 19:00:29 -0500

    Add files via upload

---
+++ Frontend/SupportSync/Vista/Usuario/escalar_casos.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../Img/img_6.png"> 
+    <title>Support Sync | Escalar Caso </title>
+
+    <link rel="stylesheet" href="../../Css/global_estilo.css">
+    <link rel="stylesheet" href="../../Css/Usuario/usuario_global_estilo.css">
+    <link rel="stylesheet" href="../../Css/Usuario/nueva_solicitud_estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="box_nav">
+      <img src="../../Img/img_3.png" alt="">
+    </nav>
+
+    <section class="box_contenedor_general">
+    <section class="box_titulo">
+        <h2 class="titulo_dos">Escalar Caso</h2>
+    </section>
+        <div class="box_formulario">
+
+            <form>
+                <h2 class="titulo_uno">Datos del Cliente</h2>
+                    <div class="row">
+                        <div class="col">
+                        <label for="sel1" class="form-label">Documento de Identificación:</label>
+                        <input type="password" class="form-control" placeholder="Documento de Identificación">
+                        </div>
+                        <div class="col">
+                        <label for="sel1" class="form-label">Nombre: </label>
+                        <input type="password" class="form-control" placeholder="Nombre" >
+                        </div>
+                    </div>
+                   <br>
+                   <h2 class="titulo_uno">Datos de consulta</h2>
+                    <div class="row">
+                        <div class="col">
+                        <label for="sel1" class="form-label">Selecciona tipo de consulta:</label>
+                            <select class="form-select" id="sel1" name="sellist1">
+                                <option>Seleccione una consulta</option>
+                                <option>Productos</option>
+                                <option>Aclaraciones de saldo</option>
+                                <option>Reclamos por cobro</option>
+                                <option>Acuerdos de pago</option>
+                            </select>   
+                        </div>
+                        <div class="col">
+                        <label for="sel1" class="form-label">Caso: </label>
+                        <input type="password" class="form-control" placeholder="Nombre" >
+                        </div>
+                    </div>
+                   
+
+                    <div class="d-flex justify-content-center box_btn_solicitud">
+                        <button type="submit" class="btn btn-dark">Consultar</button>
+                    </div>
+                </form>
+
+            </div>
+    </section>
+
+    
+
+
+</body>
+</html>
---
+++ Frontend/SupportSync/Vista/Usuario/estadisticas_asesor.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../../Img/img_6.png"> 
+    <title>Support Sync | Estadistica De Interacciones </title>
+
+    <link rel="stylesheet" href="../../Css/global_estilo.css">
+    <link rel="stylesheet" href="../../Css/Usuario/usuario_global_estilo.css">
+    <link rel="stylesheet" href="../../Css/Usuario/estadisticas_asesor_estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="../../Js/pruebas.js" ></script>
+    <script type="text/javascript" src="../../Js/prueba2.js" ></script>
+
+</head>
+<body>
+    <nav class="box_nav">
+      <img src="../../Img/img_3.png" alt="">
+    </nav>
+    
+    <section class="box_titulo">
+        <h2 class="titulo_dos">Estadistica De Interacciones</h2>
+    </section>
+
+
+    <section class="box_contenedor_general">
+
+
+        <div class="box_estadisticas">
+        <div class="col">
+                    <h2 class="titulo_uno">Consultas Resueltas</h2>
+                    <div class="box_contenedor_grafica">
+                        <div id="piechart" class="box_grafica" ></div>
+                    </div>
+                </div>
+            </div>
+            <div class="box_estadisticas">
+                <div class="col">
+                    <h2 class="titulo_uno">Promedio De Minutos Por Consulta</h2>
+                    <div class="box_contenedor_grafica">
+                        <div id="line_top_x" class="box_grafica" ></div>
+                    </div>
+                    
+                </div>
+            </div>
+           
+
+
+           
+
+        </div>
+
+
+    </section>
+
+    
+
+
+</body>
+</html>
