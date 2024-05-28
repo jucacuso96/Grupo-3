034327c4892fb66f18a2e9da89d4cf38a6103b68
Daniel-Mendez-G09 <165663616+Daniel-Mendez-G09@users.noreply.github.com>
2024-05-28 13:06:09 -0500

    Delete SupportSync directory

--- SupportSync/public/assets/js/estilo/menu.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    var btn_exp = document.querySelector('#btn_exp');
-    var menu_side = document.querySelector('.menu_lateral');
-
-    btn_exp.addEventListener('click', function() {
-        menu_side.classList.toggle('expandir');
-    });
-});
